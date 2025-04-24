f94794421dfb1d9703865d0bba59ad71be35d44a
mendozamasis123 <mendozamasisdiuiman@gmail.com>
2025-04-24 09:53:33 -0600

    Ready

--- src/Util/ConexionDB.java
+++ src/Util/ConexionDB.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class ConexionDB {
     private static final String URL="jdbc:mysql://localhost:3306/ferreteria_bd";
     private static final String USER="root";
-    private static final String PASSWORD="Alexander12";
+    private static final String PASSWORD="123456789";
     
     public static Connection getConnection() throws SQLException {
      try{
